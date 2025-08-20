cce8d29242535e03c9b74c3d962face8ded0925b
V <hello@aidocs.pro>
2025-08-20 08:33:42 +0300

    add api caller

--- Runner.java
+++ Runner.java
@@ -3,4 +3,8 @@ public class Runner {
     public void executeApiCall() {
         // TODO: this is a place holder where we will make 3rd party api call.
     }
+
+    private void callApi() {
+        // make api call
+    }
 }
